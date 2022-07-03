dc6452ed39a8c98a22f08bedee94837b9c2487bc
MD. MINHAZ <mdm047767@gmail.com>
2022-07-03 21:17:00 +0600

    created a new function called as product_url_scraper. This function takes two parameter, 1. webpage link, 2. page serial number. after call this function it is calculating the tab index for browse webpage to a specific tab. after that it is makes a get request via webdriver to the page link and wait for 5 second untill website return a valid response.

--- Harrods_Scraper/harrods_url_scraper.py
+++ Harrods_Scraper/harrods_url_scraper.py
@@ -16,3 +16,9 @@ driver = uc.Chrome(
 driver.get(base_url)
 driver.implicitly_wait(time_to_wait=2)
 driver.switch_to.new_window(type_hint='tab')
+
+def product_url_scraper(page_link: str, serial_number: int) -> None:
+    try:
+        driver.switch_to.window(driver.window_handles[serial_number % 2])
+        driver.get(page_link)
+        driver.implicitly_wait(5)
