0a77a6d703320f80e7c8878972e6a553c8dcb5f6
MD. MINHAZ <mdm047767@gmail.com>
2022-07-03 21:09:42 +0600

    Undetected Chrome Driver basic functionally added. This function have a basic url which is harrods website link. 2. object of ChromeOptions class. Call Chrome class of uc and open a new tab in browser.

--- Harrods_Scraper/harrods_url_scraper.py
+++ Harrods_Scraper/harrods_url_scraper.py
@@ -6,3 +6,13 @@ from bs4 import BeautifulSoup
 
 db = TinyDB("harrods_product_url.json")
 query = Query()
+
+base_url = "https://www.harrods.com/"
+chrome_options = uc.ChromeOptions()
+driver = uc.Chrome(
+    driver_executable_path="../chrome_driver/chromedriver",
+    options=chrome_options
+)
+driver.get(base_url)
+driver.implicitly_wait(time_to_wait=2)
+driver.switch_to.new_window(type_hint='tab')
