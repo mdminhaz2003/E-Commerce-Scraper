6272589e930c17e3876c01099a41fba106ff9310
MD. MINHAZ <mdm047767@gmail.com>
2022-07-03 21:57:38 +0600

    Making product url,             product_url = base url + itemListElement[url]             product_url = https://www.harrods.com/en-bd/shopping/shopping/max-mara-cotton-blend-single-breasted-blazer-17662826             we got this link. But this link is totally invalid. if we visit this link we won't get our desired output from webpage.             because in this link have double (/shopping/shopping) part and we need to convert it to single (/shopping) then it will work perfectly.             So let's make it. :)

--- Harrods_Scraper/harrods_url_scraper.py
+++ Harrods_Scraper/harrods_url_scraper.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 db = TinyDB("harrods_product_url.json")
 query = Query()
 
-base_url = "https://www.harrods.com/"
+base_url = "https://www.harrods.com"
 chrome_options = uc.ChromeOptions()
 driver = uc.Chrome(
     driver_executable_path="../chrome_driver/chromedriver",
@@ -26,3 +26,29 @@ def product_url_scraper(page_link: str, serial_number: int) -> None:
         soup = BeautifulSoup(markup=driver.page_source, features='html.parser')
         script_info = soup.find(name="script", attrs={'type': 'application/ld+json'})
         page_data = json.loads(script_info.text.strip())
+
+        '''
+        page data giving us a very big dictionary but we need only itemListElement part for get information about it.
+        page link : https://www.harrods.com/en-bd/shopping/women-clothing?icid=megamenu_shop_women_clothing_all-clothing
+        
+        "itemListElement":[
+            {
+                "@type":"ListItem",
+                "position":1,
+                "name":"Cotton-Blend Single-Breasted Blazer",
+                "url":"/en-bd/shopping/shopping/max-mara-cotton-blend-single-breasted-blazer-17662826"
+            },
+            ...
+        ]
+        '''
+        for product in page_data.get("itemListElement"):
+            '''
+            Making product url,
+            product_url = base url + itemListElement["url"]
+            product_url = https://www.harrods.com/en-bd/shopping/shopping/max-mara-cotton-blend-single-breasted-blazer-17662826
+            we got this link. But this link is totally invalid. if we visit this link we won't get our desired output from webpage.
+            because in this link have double (/shopping/shopping) part and we need to convert it to single (/shopping) then it will work perfectly.
+            So let's make it. :)
+            '''
+            product_url = f'{base_url}{str(product["url"]).replace("/shopping/shopping/", "/shopping")}'
+
