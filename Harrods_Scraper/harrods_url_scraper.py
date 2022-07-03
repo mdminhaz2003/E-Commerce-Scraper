4dddb85d2cff65aa89da0860f00b241a0a77f57b
MD. MINHAZ <mdm047767@gmail.com>
2022-07-03 22:24:23 +0600

    Read Comment properly.

--- Harrods_Scraper/harrods_url_scraper.py
+++ Harrods_Scraper/harrods_url_scraper.py
@@ -67,3 +67,28 @@ def product_url_scraper(page_link: str, serial_number: int) -> None:
     except Exception as unknown_exception:
         print(unknown_exception)
 
+
+'''
+For scrap https://www.harrods.com/en-bd/ website's product URL, you have to input a specific types of product page url.
+1. Visit this website : https://www.harrods.com/en-bd/
+2. Hover on specific types of product categories (Let's say Women)
+3. Click on specific types of product (Let's say Tops)
+4. Copy the url from URL bar.
+   You will get this: https://www.harrods.com/en-bd/shopping/women-clothing-tops?icid=megamenu_shop_women_clothing_tops
+   Remember, you URL must be like this. Otherwise it will not work.
+5. Paste to page_url variable.
+6. Boo oom :)
+'''
+from_page_number = 1  # From Page
+to_page_number = 50  # To Page
+
+page_url = "https://www.harrods.com/en-bd/shopping/women-clothing-tops?icid=megamenu_shop_women_clothing_tops"
+
+for page_number in range(from_page_number, to_page_number + 1):
+    page_link = f"{page_url}&pageindex={page_number}"
+    try:
+        product_url_scraper(page_link=page_link, serial_number=page_number)
+    except Exception as exception:
+        print(exception)
+
+print("Program Finished...")
